1e1300c5a975d8f0ec22fff0274b09e1515f825c
matthew.chudleigh <matthew.chudleigh@entaingroup.com.au>
2026-01-30 00:58:42 +0000

    Fix KeyError in reverse_relations when processing module resources
    Used stripped node name (without module prefix) as graphdict key instead of the full resource name.
    The graphdict uses full names like "module.base.aws_vpc.main", not stripped names like "aws_vpc.main".

--- modules/graphmaker.py
+++ modules/graphmaker.py
@@ -351,7 +351,7 @@ def reverse_relations(tfdata: Dict[str, Any]) -> Dict[str, Any]:
             if reverse_origin:
                 if n not in tfdata["graphdict"][c]:
                     tfdata["graphdict"][c].append(n)
-                tfdata["graphdict"][node].remove(c)
+                tfdata["graphdict"][n].remove(c)
 
     return tfdata
 
